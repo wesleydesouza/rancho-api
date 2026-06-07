17ff6afac6a43c7634fe3590971539cb438590dd
Wesley <wesley.duarte@ikatec.com.br>
2026-06-07 11:42:22 -0300

    feat: adiciona rotas

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
-const cors = require("cors");
 const express = require("express");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const futurePurchases = require("./routes/futurePurchases");
+const listPurchases = require("./routes/listPurchases");
+
+app.use("/futurePurchases", futurePurchases);
+app.use("/listPurchases", listPurchases);
 
 app.get("/", (req, res) => {
   res.json({ status: "ok" });
