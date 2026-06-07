78563f1bc56859122268f4db4db31f3685a785f3
Wesley <wesley.duarte@ikatec.com.br>
2026-06-07 11:30:47 -0300

    fix: simplicifa index

--- api/index.js
+++ api/index.js
@@ -1,36 +1,13 @@
 const cors = require("cors");
 const express = require("express");
-const mongoose = require("mongoose");
 
 const app = express();
 
-const futurePurchases = require("./routes/futurePurchases");
-const listPurchases = require("./routes/listPurchases");
-
 app.use(cors());
-
-app.use(
-  express.urlencoded({
-    extended: true,
-  }),
-);
-
 app.use(express.json());
 
-app.use("/futurePurchases", futurePurchases);
-app.use("/listPurchases", listPurchases);
-
-mongoose.set("strictQuery", true);
-
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8yzgyyv.mongodb.net/?retryWrites=true&w=majority`,
-  )
-  .then(() => {
-    console.log("Conectamos ao MongoDB!");
-  })
-  .catch((error) => {
-    console.error("Erro ao conectar ao MongoDB:", error);
-  });
+app.get("/", (req, res) => {
+  res.json({ status: "ok" });
+});
 
 module.exports = app;
